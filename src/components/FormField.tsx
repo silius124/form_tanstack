a83d402b57ab8afd08ab32d7593284e48de315ab
silius124 <silius125@yahoo.com>
2026-04-16 20:24:11 +0300

    feat: print error in formfield component

--- src/components/FormField.tsx
+++ src/components/FormField.tsx
@@ -1,28 +1,21 @@
 import type { ReactNode } from "react";
-import { useFormContext } from "react-hook-form";
 
 interface FormFieldProps {
   label: string;
   name: string;
+  error: string | undefined;
   children: ReactNode;
 }
 
-function FormField({ label, name, children }: FormFieldProps) {
-  const {
-    formState: { errors },
-  } = useFormContext();
-  const errorMessage =
-    errors[name]?.message || (errors[name] ? "Поле обязательно" : "");
+function FormField({ label, name, error, children }: FormFieldProps) {
   return (
     <div className="grid grid-cols-[minmax(100px,auto)_1fr] gap-x-4 items-center w-full max-w-lg">
       <label htmlFor={name}>{label}:</label>
       <div className="flex flex-col">
         {" "}
         {children}
-        {errorMessage && (
-          <span className="text-red-500 text-xs tracking-wide">
-            {errorMessage}
-          </span>
+        {error && (
+          <span className="text-red-500 text-xs tracking-wide">{error}</span>
         )}
       </div>
     </div>
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -11,7 +11,7 @@ function Input({ type, name }: InputProps) {
     <input
       type={type}
       id={name}
-      {...register(name, { required: true })}
+      {...register(name)}
       className="text-white px-3 py-2 w-xs bg-blue-600/30 backdrop-blur-md rounded-md border border-white/20 shadow-lg outline-none transition-all duration-200 focus:border-white/40 "
     />
   );
