50d937f0a405c0a30fcf4f9c6f793ded7c7fc126
silius124 <silius125@yahoo.com>
2026-04-16 17:26:48 +0300

    style: remove old shadow style

--- src/components/ButtonPrimary.tsx
+++ src/components/ButtonPrimary.tsx
@@ -1,6 +1,6 @@
 function ButtonPrimary({ text }: { text: string }) {
   return (
-    <button className="text-xl px-6 py-2 w-fit text-white bg-blue-600/30 backdrop-blur-md rounded-md border  border-white/20 shadow-[inset_0_1px_1px_rgba(255,255,255,0.3)] shadow-lg transition-all duration-200 hover:border-white/40">
+    <button className="text-xl px-6 py-2 w-fit text-white bg-blue-600/30 backdrop-blur-md rounded-md border  border-white/20 shadow-lg transition-all duration-200 hover:border-white/40">
       {text}
     </button>
   );
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -8,7 +8,7 @@ function Input({ type, name }: InputProps) {
     <input
       type={type}
       id={name}
-      className="text-white px-3 py-2 bg-blue-600/30 backdrop-blur-md rounded-md border border-white/20 shadow-[inset_0_1px_1px_rgba(255,255,255,0.3)] shadow-lg outline-none transition-all duration-200 focus:border-white/40 "
+      className="text-white px-3 py-2 bg-blue-600/30 backdrop-blur-md rounded-md border border-white/20 shadow-lg outline-none transition-all duration-200 focus:border-white/40 "
     />
   );
 }
