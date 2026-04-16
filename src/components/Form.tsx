9165aa12f3612839ce5aa68b7c65dd2044156513
silius124 <silius125@yahoo.com>
2026-04-16 20:31:27 +0300

    ref: remove onSubmit type in Form component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,7 +3,7 @@ import ButtonPrimary from "./ButtonPrimary";
 
 interface FormProps {
   title: string;
-  onSubmit: (e: React.SubmitEvent<HTMLFormElement>) => void;
+  onSubmit: () => void;
   children: ReactNode;
 }
 
@@ -12,7 +12,7 @@ function Form({ title, onSubmit, children }: FormProps) {
     <>
       <form
         className="flex flex-col gap-10 items-center text-white bg-slate-900/60 backdrop-blur-md rounded-xl border border-white/20 shadow-[inset_0_1px_1px_rgba(255,255,255,0.15)] p-6 w-auto"
-        onSubmit={(e: React.SubmitEvent<HTMLFormElement>) => onSubmit(e)}
+        onSubmit={onSubmit}
       >
         {title && <h2 className="text-4xl text-center">{title}</h2>}
         <div className=" gap-5 flex flex-col">{children}</div>
